658cd84d8d89564af681ba756ce1c6165c3c0fa8
프렉탈 <csnam515@gmail.com>
2019-08-15 09:18:30 +0900

    Add files via upload
    first

---
+++ app_file.js
@@ -0,0 +1,31 @@
+
+var express = require('express');
+var app = express(); // 객체를 만든다.// 
+const hostname = '127.0.0.1';
+const port = 3000;
+//app.set('views','./views_files');
+//app.set('view engine','jade');
+app.get('/topic/new',function(req,res){
+    res.send('hi');
+});
+app.listen(port,hostname,function(){
+    console.log('connected 3000 port');
+});
+
+
+// http 예제 
+/*
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text/plain'});
+    res.end('hello csnam');
+}).listen(port,hostname,function(){
+    console.log(`Server running at http://${hostname}:${port}`);
+});
+*/
+
+
+
+
