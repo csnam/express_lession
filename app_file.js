5649538af96bbfc6a039bc0ecc6075aba1f93428
csnam <csnam515@gmail.com>
2019-08-15 15:15:40 +0900

    제목와 내용을 표출하기

--- app_file.js
+++ app_file.js
@@ -22,7 +22,24 @@ app.get('/topic',function(req,res){
         }
         res.render('view',{topics:files});  // files객체를 넣음.
     })
- })
+ });
+app.get('/topic/:id',function(req,res){    // id를 읽어와서 처리
+    var id = req.params.id;
+    fs.readdir('data',function(err,files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        fs.readFile('data/'+ id,'utf-8',function(err,data){
+            if (err){
+                console.log(err);
+                res.status(500).send("Read Error");
+            }
+            res.render('view',{topics:files,title:id,description:data});
+        });
+    });
+    
+})
 app.post('/topic',function(req,res){
     var title = req.body.title;
     var description = req.body.description;
---
+++ readme.txt
@@ -0,0 +1,14 @@
+
+https://github.com/egoing/server_side_javascript_tutorials
+
+npm install express --save
+npm install jade --save
+npm install body-parser --save
+
+// http://jade-lang.com/reference/iteration
+
+
+
+//컴퓨터\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\i8042prt\Parameters
+'LayerDriver KOR  =>'kbd101c.dll' 
+'OverrideKeyboardSubtype => 5
