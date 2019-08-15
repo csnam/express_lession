f8241a75f2fe095c341b776ddf943770ed3db094
csnam <csnam515@gmail.com>
2019-08-15 11:23:16 +0900

    post 데이타 올리기
    1.jade 파일로 html를 작성 , textbox, texarea, button  making
    2./topic 주소를 action 으로 하고 post router를 구현함 (app.post('topic)),
     body-parser객체를 로도해야함.
    3.file로 cotrol 내용을 적고 에러시 방어코드 작성

--- app_file.js
+++ app_file.js
@@ -1,16 +1,35 @@
 
 var express = require('express');
+var bodyParser = require('body-parser');  // // post 가능하게 해줌
 var app = express(); // 객체를 만든다.//  간단한 수정
+var fs = require('fs'); // file system 사용 
 const hostname = '127.0.0.1';
 const port = 3000;
-//app.set('views','./views_files');
-//app.set('view engine','jade');
+app.locals.pretty = true;
+app.set('views','./views_files');
+app.set('view engine','jade');
+
+app.use(bodyParser.urlencoded({ extended: false })) 
 app.get('/topic/new',function(req,res){
-    res.send('hi');
+    res.render('new');
 });
+
+
+app.post('/topic',function(req,res){
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/'+ title+'.txt',description,function(err){
+        if (err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');// send  다음에는 실행되지 않는다.
+        }
+        res.send('Success ~~');
+    });
+    //res.send('hi post :' + req.body.title + req.body.description);
+})
 app.listen(port,hostname,function(){
     console.log('connected 3000 port');
-});
+}); 
 
 
 // http 예제 
